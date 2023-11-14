0475bcc4b7f65c0bfda4b1767ecd14c30a1cec2e
RevaNayla <revanano11@gmail.com>
2023-11-14 23:50:04 +0700

    web pribadi revanayla uts promnet

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Biodata from './components/biodata/Biodata';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+
+
+
+const App = () => {
+  return (
+    <div>
+  
+    <Router>
+    <Header />
+      <Routes>
+      <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/biodata' element={<Biodata />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    <Footer />
+    <ScrollUp />
+    </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Sosial.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sosial = () => {
+    return (
+        <div className="home__sosial">
+            <a href="https://instagram.com/" className="home__sosial-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://twitter.com/" className="home__sosial-icon" target="_blank">
+            <i class="uil uil-twitter-alt"></i>
+            </a>
+            <a href="https://github.com/" className="home__sosial-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Sosial;
